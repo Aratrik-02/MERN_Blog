0cb4e6312e4b7138724e82317970229c1acd4ddd
unknown <spryaratrik.basak@gmail.com>
2023-07-11 19:04:08 +0530

    Some changes

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -34,28 +34,29 @@ function Login() {
   return (
     <div className='signup_container'>
         <div className='signup_form'>
-            <h2>Login </h2>
+            <h2 className="login-header">Login </h2>
             <br />
             <form onSubmit={handleSubmit}>
                 <div className='user-box'>
-                    <label htmlFor="email"></label><br />
-                    <input type="email" placeholder='Email'
+                    {/* <label htmlFor="email">Email</label><br /> */}
+                    <input className="login-input" type="email" placeholder='Email'
                     onChange={e => setEmail(e.target.value)}
                     />
                 </div>
                 <br />
                 <div className='user-box'>
-                    <label htmlFor="password"></label><br />
-                    <input type="password" placeholder='Password'
+                    {/* <label htmlFor="password"></label><br /> */}
+                    <input type="password" placeholder='Password' className="login-input"
                     onChange={e => setPassword(e.target.value)}
                     />
                 </div>
                 <button className='signup_btn'>Login</button>
             </form>
-            {/* <br/>
-            <br/>
-            <p>Not Registered?</p> */}
-            <Link to="/register"><button>Signup</button></Link>
+            <p className='not-register'>Not registered yet?
+            <Link className="signup_redirect" to="/register">
+                Signup
+            </Link>
+            </p>
         </div>
     </div>
   )
